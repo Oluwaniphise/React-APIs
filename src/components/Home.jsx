67ef4253bc2285fde7e5dae64b44707dd4e92357
Oluwaniphise <oduyaleenoch@gmail.com>
2022-07-04 14:38:06 +0100

    refactoring my apis into pages

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,12 @@ import {Link} from 'react-router-dom'
 export const Home = () => {
     return (
         <section className='bg-gray-200 h-[100vh] py-4 text-center flex flex-col items-center'>
-            <p className='max-w-[600px] p-2'>
+            <p className='max-w-[600px] font-[18px] p-2 px-5'>
                 Hi, Welcome to my API hub. Herein, you get to see the list of API projects I've worked on using Reactjs.
                 Feel free to play around.
             </p>
 
-            <ul className=''>
+            <ul className='mt-4'>
                 <li>
                     <Link className='underline' to="/Weather">Weather API</Link>
                 </li>
