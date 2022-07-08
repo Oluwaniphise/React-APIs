bd1f6995fddd306c80eb2513b96b205429139677
Oluwaniphise <oduyaleenoch@gmail.com>
2022-07-08 17:17:05 +0100

    added Github route

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,13 @@ import {Link} from 'react-router-dom'
 
 export const Home = () => {
     return (
-        <section className='bg-gray-200 h-[100vh] py-4 text-center flex flex-col items-center'>
+        <section className='bg-gray-200 h-[100vh] py-4  flex flex-col items-center'>
             <p className='max-w-[600px] font-[16px] p-2 px-5'>
                 Hi, I'm Oduyale Enoch, a front end developer. Welcome to my API hub. Herein, you get to see the list of API projects I've worked on using Reactjs.
                 Feel free to play around.
             </p>
 
-            <ul className='mt-4'>
+            <ol className='mt-4 list-disc'>
                 <li>
                     <Link className='underline' to="/github">Github Search API</Link>
                 </li>
@@ -25,7 +25,7 @@ export const Home = () => {
                 <li>
                     <Link className='underline' to="/mealdb">Meal database</Link>
                 </li>
-            </ul>
+            </ol>
 
             <em className='mt-4'>More coming soon</em>
 
